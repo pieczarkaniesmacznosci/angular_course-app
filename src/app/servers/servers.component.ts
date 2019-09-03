216da03a256a039f71a4015ddba7c8f4cdddc5f2
Lukasz Pieczonka <lukasz.pieczonka@globallogic.com>
2019-09-03 15:38:53 +0200

    success-alert and warning-alert components created

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  // selector: '[app-servers]',// - accessed through attribute, 
+  selector: '.app-servers',// accessed through class 'app-servers' 
+  // selector: 'app-servers'// - accessed through html tag <app-servers>
   templateUrl: './servers.component.html',// has to be present at all times. either template or templateUrl - it refers to html doc template: '<app-server></app-server>'
   styleUrls: ['./servers.component.css']
 })
