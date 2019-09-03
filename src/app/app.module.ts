ac35dbce7f429861b8cbee12440e2cf2f13cc6f7
Lukasz Pieczonka <lukasz.pieczonka@globallogic.com>
2019-09-03 13:43:04 +0200

    servers component add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
+import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent //Registering component so Angular know it exists
+    ServerComponent, //Registering component so Angular know it exists
+    ServersComponent
   ],
   imports: [
     BrowserModule
