eec7a8c138da164d8949fa0bce542b41b5d3c8fe
Lukasz Pieczonka <lukasz.pieczonka@globallogic.com>
2019-09-03 16:03:51 +0200

    string interpolation example

--- src/app/success-alert/success-alert.component.ts
+++ src/app/success-alert/success-alert.component.ts
@@ -1,11 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-success-alert',
-    templateUrl:'./success-alert.component.html',
-    styleUrls: ['./success-alert.component.css']
+  selector: 'app-success-alert',
+  templateUrl: './success-alert.component.html',
+  styleUrls: ['./success-alert.component.css']
 })
-
-export class SuccessAlertComponent{
-
-}
+export class SuccessAlertComponent {}
