03632bb86acd101e17c6f8827262ceaecca5f7b7
Lukasz Pieczonka <lukasz.pieczonka@globallogic.com>
2019-09-03 13:55:46 +0200

    temlate and style placing, either in ts or seperate file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  // styleUrls: ['./app.component.css']
+  styles:[`
+    h3{
+      color: dodgerblue;
+    }
+  `] // style or style URL can be added directly in ts file.
 })
 export class AppComponent {
   name = 'Lukasz';
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
+  templateUrl: './servers.component.html',// has to be present at all times. either template or templateUrl - it refers to html doc template: '<app-server></app-server>'
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
