d195e7f2f8bed400a8ad9807bc558cb29dc19cce
Lukasz Pieczonka <lukasz.pieczonka@globallogic.com>
2019-09-03 22:53:07 +0200

    Two way binding with use of ngModel directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,18 @@ import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
 import { WarningAlertComponent } from './warningAlert/warning-alert.component';
 import { SuccessAlertComponent } from './success-alert/success-alert.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent, //Registering component so Angular know it exists
-    ServersComponent, WarningAlertComponent, SuccessAlertComponent
+    ServersComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
