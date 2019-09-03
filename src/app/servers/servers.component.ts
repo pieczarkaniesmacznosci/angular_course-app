65767725cec17657f76a887504d3c652d9268869
Lukasz Pieczonka <lukasz.pieczonka@globallogic.com>
2019-09-03 16:35:08 +0200

    property binding - button disabled attribute

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,17 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  // selector: '[app-servers]',// - accessed through attribute, 
-  selector: '.app-servers',// accessed through class 'app-servers' 
+  // selector: '[app-servers]',// - accessed through attribute,
+  selector: '.app-servers', // accessed through class 'app-servers'
   // selector: 'app-servers'// - accessed through html tag <app-servers>
-  templateUrl: './servers.component.html',// has to be present at all times. either template or templateUrl - it refers to html doc template: '<app-server></app-server>'
+  templateUrl: './servers.component.html', // have to be,either template or templateUrl- refers to html doc template:'<app-server></...>'
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    setTimeout(allowNewServer => {
+      this.allowNewServer = true;
+    }, 2000);
   }
 
+  ngOnInit() {}
 }
